a8253342b2679cc3269cdd6c79c7f3c57f8f984c
Tetiana Bronitska <tetiana.bron@gmail.com>
2021-10-29 01:21:01 +0300

    Changed in API

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'https://restcountries.eu/rest/v2';
+// const BASE_URL = 'https://restcountries.eu/rest/v2';
+const BASE_URL = 'https://restcountries.com/v2/';
 
 export default function fetchCountries(searchQuery) { 
     return fetch(`${BASE_URL}/name/${searchQuery}`)
